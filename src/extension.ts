bf77f6f0cd3ca929b2e21a612b9d86e67a54c6d9
Alex Chan <alexander.m.chan@gmail.com>
2024-02-24 10:13:48 -0700

    clean up comments

--- src/extension.ts
+++ src/extension.ts
@@ -57,7 +57,6 @@ export function simpleCodeReplacer({
   const regex = /["`'](.*?)["'`]/gim
   const inputString = code
 
-  console.log({ code })
   // Replacement function
   const replacedString = inputString.replace(regex, (match, captureGroup) => {
     // Replace captured group with desired replacement
@@ -128,10 +127,6 @@ let lastPrefix = 'tw-'
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "prefixclass" is now active!')
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
